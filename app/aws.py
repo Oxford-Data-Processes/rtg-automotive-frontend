c7f16fb6fc07c5dc962e21363762b13d373d0174
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-16 16:07:38 +0100

    Athena works

--- app/aws.py
+++ app/aws.py
@@ -36,40 +36,33 @@ def get_last_csv_from_s3(bucket_name, prefix, s3_client):
     return csv_files[0]["Key"] if csv_files else None
 
 
-# def load_csv_from_s3(bucket_name, csv_key, s3_client):
-#     csv_object = s3_client.get_object(Bucket=bucket_name, Key=csv_key)
-#     csv_data = csv_object["Body"].read()
-#     df = pd.read_csv(BytesIO(csv_data))
-#     return df
+# def run_athena_query(query):
+#     # Initialize a session using Boto3
+#     session = boto3.Session()
+#     athena_client = session.client("athena", region_name="eu-west-2")
 
+#     # Define the parameters for the query execution
+#     response = athena_client.start_query_execution(
+#         QueryString=query,
+#         QueryExecutionContext={"Database": "rtg_automotive"},
+#         WorkGroup="rtg-automotive-workgroup",
+#     )
 
-def run_athena_query(query):
-    # Initialize a session using Boto3
-    session = boto3.Session()
-    athena_client = session.client("athena", region_name="eu-west-2")
+#     query_execution_id = response["QueryExecutionId"]
+#     # Wait for the query to complete
+#     while True:
+#         query_status = athena_client.get_query_execution(
+#             QueryExecutionId=query_execution_id
+#         )
+#         status = query_status["QueryExecution"]["Status"]["State"]
+#         if status in ["SUCCEEDED", "FAILED", "CANCELLED"]:
+#             break
 
-    # Define the parameters for the query execution
-    response = athena_client.start_query_execution(
-        QueryString=query,
-        QueryExecutionContext={"Database": "rtg_automotive"},
-        WorkGroup="rtg-automotive-workgroup",
-    )
-
-    query_execution_id = response["QueryExecutionId"]
-    # Wait for the query to complete
-    while True:
-        query_status = athena_client.get_query_execution(
-            QueryExecutionId=query_execution_id
-        )
-        status = query_status["QueryExecution"]["Status"]["State"]
-        if status in ["SUCCEEDED", "FAILED", "CANCELLED"]:
-            break
-
-    if status == "SUCCEEDED":
-        return athena_client.get_query_results(QueryExecutionId=query_execution_id)
-    else:
-        st.error(f"Query failed with status: {status}")
-        return []
+#     if status == "SUCCEEDED":
+#         return athena_client.get_query_results(QueryExecutionId=query_execution_id)
+#     else:
+#         st.error(f"Query failed with status: {status}")
+#         return []
 
 
 def trigger_lambda_function(function_name: str, aws_account_id: str) -> bool:
