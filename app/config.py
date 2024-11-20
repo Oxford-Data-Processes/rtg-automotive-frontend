c478f4d788872530031fc52261190eb82ce849a3
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-20 19:28:34 +0000

    Checkpoint

--- app/config.py
+++ app/config.py
@@ -30,20 +30,25 @@ def main():
             if st.button("Save Config"):
                 try:
                     updated_data = json.loads(updated_config)
+                    s3_handler.upload_json_to_s3(
+                        PROJECT_BUCKET_NAME, json_key, updated_data
+                    )
                     st.success("Config updated successfully!")
                 except json.JSONDecodeError:
                     st.error("Invalid JSON format.")
 
         with tab_functions:
             st.subheader("Functions")
-            functions_list = [
-                "set_value_to_10_if_labelled_yes",
-                "get_value_if_less_than_10_else_0",
-                "set_value_to_10_if_labelled_in_stock",
-                "set_value_to_10_if_product_in_list",
-            ]
-            for function in functions_list:
-                st.write(f"{function}")
+            functions_dictionary = {
+                "set_value_to_10_if_labelled_yes": "Returns 10 if 'yes' is found in the input string, otherwise returns 0.",
+                "get_value_if_less_than_10_else_0": "Returns the input value if it's less than or equal to 10, otherwise returns 0.",
+                "set_value_to_10_if_labelled_in_stock": "Returns 10 if the input is 'in stock', otherwise returns 0.",
+                "set_value_to_10_if_product_in_list": "Always returns 10 regardless of input.",
+            }
+            for function, description in functions_dictionary.items():
+                st.markdown(f"### `{function}`")
+                st.markdown(f"> **Description:** {description}")
+                st.markdown("---")  # Adds a horizontal line for better separation
 
     except Exception as e:
         st.error(f"Error loading config: {str(e)}")
